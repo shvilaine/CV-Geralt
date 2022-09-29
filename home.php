95a68c70484a56946749f2d271b742f8ac6bbcd1
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2022-09-29 14:18:02 +0200

    modif 29/09

--- home.php
+++ home.php
@@ -5,15 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Website with an online resume of Geralt of Rivia, The Witcher">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="burger.css">
     <title>Geralt's Resume - Home</title>
 </head>
 <body>
-    <nav>
-        <div class="burger"></div>
+    <nav class="navbar dark-mode" role="navigation">
         <div class="navtitle"> <?php include('navbar.php') ?> </div>
-        <div class="message"></div>
+        
+        <ul class="navbar__links">
+      <li class="navbar__link first"><a href="#">Home</a></li>
+      <li class="navbar__link second"><a href="#section2">Experiences</a></li>
+      <li class="navbar__link third"><a href="#">Missions</a></li>
+      <li class="navbar__link four"><a href="#">Portfolio</a></li>
+      <li class="navbar__link fifth"><a href="#">Contact</a></li>
+    </ul>   
+    <button class="burger">
+      <span class="bar"></span>  
+    </button>   
     </nav>
+   
     <header class="background-image">
         <div class="photo">
             <h1>Geralt of Rivia</h1>
@@ -24,11 +35,12 @@
     <div class="networks">
         <h1>My social networks</h1>
     </div>
-
+    
+ 
 
 <footer>
     <?php include('footer.php') ?>
 </footer>
-
+<script type="text/javascript" src="burger.js"></script>
 </body>
 </html>
