3800963ee3a6d1c930041b44ef45419b8fbb2633
DRISS BOUMEDIENE <driss308@hotmail.fr>
2022-09-29 14:14:55 +0200

    commit 29/09 début aprem

--- home.php
+++ home.php
@@ -7,25 +7,36 @@
     <meta name="description" content="Website with an online resume of Geralt of Rivia, The Witcher">
     <link rel="stylesheet" href="style.css">
     <title>Geralt's Resume - Home</title>
+    <script src="https://kit.fontawesome.com/50a5351433.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <nav>
         <div class="burger"></div>
-        <div class="navtitle"> <?php include('navbar.php') ?> </div>
+        <div class="navtitle" class="clickable"> <?php include('navbar.php') ?> </div>
         <div class="message"></div>
     </nav>
-    <header class="background-image">
-        <div class="photo">
-            <h1>Geralt of Rivia</h1>
+
+    <header>
+        <div class="background-image">
+            <div class="photo">
+                <h1>Geralt of Rivia</h1>
+                <h2>Professional Witcher and Web Developer</h2>
+            </div>
         </div>
-</header>
+    </header>
 
     
-    <div class="networks">
+    <section class="networks">
         <h1>My social networks</h1>
-    </div>
-
-
+            <div class="icons">
+                <a href="#"><p><i class="fa-brands fa-linkedin"></i></p></a>
+                <a href="#"><p><i class="fa-brands fa-square-facebook"></i></p></a>
+                <a href="#"><p><i class="fa-brands fa-square-twitter"></i></p></a>
+                <a href="#"><p><i class="fa-brands fa-instagram"></i></p></a>
+                <a href="#"><p><i class="fa-brands fa-square-github"></i></p></a>
+            </div>
+</section>
+    
 <footer>
     <?php include('footer.php') ?>
 </footer>
