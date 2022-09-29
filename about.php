32ae8af7d508b2a4ed3707b59b670f168562138a
DRISS BOUMEDIENE <driss308@hotmail.fr>
2022-09-29 19:39:35 +0200

    début About

---
+++ about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Website with an online resume of Geralt of Rivia, The Witcher">
+    <link rel="stylesheet" href="style2.css">
+    <title>Geralt's Resume - About me</title>
+    </head>
+
+<body>
+    <nav>
+        <div class="burger"></div>
+        <div class="navtitle"> <h1>ABOUT ME</h1> </div>
+        <div class="message"></div>
+    </nav>
+
+    <header>
+        <div class="photo"></div>
+        <h1>Geralt of Rivia</h1>
+    </header>
+
+    
+
+
+
+
+</body>
+</html>
