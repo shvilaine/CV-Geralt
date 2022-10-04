24d1474cccb4ae2ea5a426db2c8d1e6d4d451d7f
DRISS BOUMEDIENE <driss308@hotmail.fr>
2022-10-04 12:41:01 +0200

    page about presque finie, expérience et compétences commencées

--- experience.php
+++ experience.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Website with an online resume of Geralt of Rivia, The Witcher">
-    <link rel="stylesheet" href="style3.css">
-    <title>Geralt's Resume - Experience</title>
+    <link rel="stylesheet" href="experience.css" href="base.css">
+    <title>CV de Geralt - Expérience</title>
     </head>
 
 <body>
@@ -18,38 +18,30 @@
 
     <header>
         <div class="photo"></div>
-        <h1>Geralt of Rivia</h1>
+        <h1>Geralt de Riv</h1>
     </header>
 
 <section class="experiences">
 
 
-    <h1>Master Web Developer</h1><h2>- Pont Vanis, Kovir </h2>
-    <ul>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-    </ul>
+    <h1>Developpeur Web</h1><h2>- Pont Vanis, Kovir </h2>
+    <p>Suite à l'obtention de mon diplôme de développeur, j'ai eu la chance de travailler
+        pour le compte du roi du Kovir, qui m'a chargé de développer le site officiel du royaume.
+        Mon travail a permis d'augmenter le trafic du site de 30% dans tous les royaumes du Nord.</p>
     <hr>
-    <h1>Web Developer Apprentice</h1><h2>- Oxenfurt, Redania</h2>
-    <ul>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-    </ul>
-    <hr>
-<h1>Master Witcher</h1><h2>- Northern Kingdoms</h2>
-    <ul>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-    </ul>
-<hr>
-<h1>Witcher Apprentice</h1><h2>- Northern Kingdoms</h2>
-<ul>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum vero laboriosam quaerat natus laborum exercitationem</li>
-    </ul>
 
+    <h1>Apprenti Developpeur</h1><h2>- Wild Code School, Oxenfurt, Redania</h2>
+    <p>Après en avoir fini avec mes pérégrinations de sorceleur, j'ai obtenu une bourse à la célèbre
+        Wild Code School de l'université d'Oxenfurt, dans laquelle j'ai suivi une formation de développeur
+        web en PHP et JavaScript.</p>
+
+    <hr>
+<h1>Maitre Sorceleur</h1><h2>- Monde entier</h2>
+    <p>A l'issue de ma formation, j'ai parcouru le monde entier pendant des années au fil des contrats, et aussi des (més)aventures de Ciri,
+       ma fille adoptive, et Yennefer, ma... je ne sais pas trop, demandez-lui.<hr>
+
+<h1>Apprenti Sorceleur</h1><h2>- Kaer Morhen</h2>
+    <p>Arrivé enfant dans cette magnifique (horrible) forteresse de Kaer Morhen,
+        j'y ai appris le combat, l'alchimie, et tous les rudiments du métier de sorceleur
+        auprès de Vesemir, mon mentor de toujours.
 </section>
---
+++ skills.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Website with an online resume of Geralt of Rivia, The Witcher">
+    <link rel="stylesheet" href="about.css" href="base.css">
+    <title>CV de Geralt - Compétences</title>
+    </head>
+
+<body>
+    <nav>
+        <div class="burger"></div>
+        <div class="navtitle"> <h1>COMPETENCES</h1> </div>
+        <div class="message"></div>
+    </nav>
+
+    <header>
+        <div class="photo"></div>
+        <h1>Geralt de Riv</h1>
+    </header>
+
+    <section class="skills">
+
+    
+    <h1>Compétences professionnelles</h1>
+    <h2>PHP</h2>
+    <h2>Combat à l'épée</h2>
+    <h2>JavaScript</h2>
+    <h2>Combat à mains nues</h2>
+    <h2>Alchimie</h2>
+    <h2>HTML / CSS</h2>
+    <h1>Soft Skills</h1>
+    <h2>Rigueur et fiabilité</h2>
+    <h2>Résistance au stress</h2>
+    <h2>Maîtrise de soi</h2>
+    <h2>Communication et amabilité</h2>
+    
+
+
+    </section>
