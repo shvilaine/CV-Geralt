0ddbd33e7ddff5d59068038e6df5d687c3d64ea1
DRISS BOUMEDIENE <driss308@hotmail.fr>
2022-10-07 12:36:41 +0200

    tableaux passés en php

--- skills.php
+++ skills.php
@@ -1,3 +1,31 @@
+<?php
+
+$devSkills = [
+    'PHP' => '90',
+    'HTML' => '80',
+    'MySQL' => '90',
+    'CSS' => '40',
+    'JavaScript' => '70',
+];
+
+$witcherSkills = [
+    'Chasse aux monstres' => '90',
+    'Alchimie' => '65',
+    'Combat à l\'épée' => '90',
+    'Équitation' => '80',
+    'Combat à mains nues' => '85',
+];
+
+$softSkills = [
+    'Rigueur et fiabilité' => '90',
+    'Créativité' => '70',
+    'Résistance au stress' => '95',
+    'Maîtrise de soi' => '85',
+    'Amabilité' => '35',
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,10 +43,10 @@
 <body>
     <nav class="navbar dark-mode" role="navigation">
         <div></div>
-        <h1 class="navtitle">COMPETENCES</h1> 
+        <h1 class="navtitle">COMPETENCES</h1>
 
         <?php include('navbar.php') ?>
-        
+
     </nav>
 
     <header>
@@ -28,37 +56,16 @@
 
     <section class="skills">
 
-    <h1>Competences de developpeur</h1>
-    <div class="skills-bar">
-        <div class="bar2">
-            <div class="info">
-                <span>PHP</span>
-            </div>
-            <div class="progress-line php"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>HTML</span>
-            </div>
-            <div class="progress-line html"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>MySQL</span>
-            </div>
-            <div class="progress-line mysql"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>CSS</span>
-            </div>
-            <div class="progress-line css"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>JavaScript</span>
-            </div>
-            <div class="progress-line js"><span></span></div>
+        <h1>Competences de developpeur</h1>
+        <div class="skills-bar">
+            <?php foreach ($devSkills as $devSkill => $progress) : ?>
+                <div class="bar2">
+                    <div class="info">
+                        <span><?= $devSkill ?></span>
+                    </div>
+                    <div class="progress-line" style="--progress-width:<?= $progress . '%' ?>"><span data-progress="<?= $progress ?>%"></span></div>
+                </div>
+            <?php endforeach ?>
         </div>
 
         <div class="picture">
@@ -67,37 +74,16 @@
         </div>
         <hr>
 
-    <h1>Competences de sorceleur</h1>
-    <div class="skills-bar">
-        <div class="bar2">
-            <div class="info">
-                <span>Chasse aux monstres</span>
-            </div>
-            <div class="progress-line hunt"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Alchimie</span>
-            </div>
-            <div class="progress-line alchimie"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Combat à l'épée</span>
-            </div>
-            <div class="progress-line epee"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Équitation</span>
-            </div>
-            <div class="progress-line equitation"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Combat à mains nues</span>
-            </div>
-            <div class="progress-line bagarre"><span></span></div>
+        <h1>Competences de sorceleur</h1>
+        <div class="skills-bar">
+        <?php foreach ($witcherSkills as $witcherSkill => $wProgress) : ?>
+                <div class="bar2">
+                    <div class="info">
+                        <span><?= $witcherSkill ?></span>
+                    </div>
+                    <div class="progress-line" style="--progress-width:<?= $wProgress . '%' ?>"><span data-progress="<?= $wProgress ?>%"></span></div>
+                </div>
+            <?php endforeach ?>
         </div>
 
         <div class="picture">
@@ -106,45 +92,24 @@
         </div>
         <hr>
 
-    <h1>Soft Skills</h1>
+        <h1>Soft Skills</h1>
 
-       <div class="skills-bar">
-        <div class="bar2">
-            <div class="info">
-                <span>Rigueur et fiabilité</span>
-            </div>
-            <div class="progress-line rigor"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Créativité</span>
-            </div>
-            <div class="progress-line creativity"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Résistance au stress</span>
-            </div>
-            <div class="progress-line stress"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Maîtrise de soi</span>
-            </div>
-            <div class="progress-line maitrise"><span></span></div>
-        </div>
-        <div class="bar2">
-            <div class="info">
-                <span>Amabilité</span>
-            </div>
-            <div class="progress-line communication"><span></span></div>
+        <div class="skills-bar">
+        <?php foreach ($softSkills as $softSkill => $softProgress) : ?>
+                <div class="bar2">
+                    <div class="info">
+                        <span><?= $softSkill ?></span>
+                    </div>
+                    <div class="progress-line" style="--progress-width:<?= $softProgress . '%' ?>"><span data-progress="<?= $softProgress ?>%"></span></div>
+                </div>
+            <?php endforeach ?>
         </div>
 
     </section>
     <div class="btn"><i class="icon"></i>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="btn" viewBox="0 0 16 16">
-  <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
-</svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="btn" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+        </svg>
     </div>
 
     <footer>
