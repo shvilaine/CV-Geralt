3ba6d24083863179563669aea2cce1f8da268c6c
Severine Helene Vilaine <severine.vilaine@gmail.com>
2022-10-07 10:28:41 +0200

    All done, can be pushed onto main

--- contact_form.php
+++ contact_form.php
@@ -30,13 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     if (empty($contact['message'])) {
         $wrongInputs[] = 'Hmm. Le message est obligatoire.';
     }
-
-    if (empty($wrongInputs)) {
-        header('Location: contact_form.php');
-    }
 }
-
-var_dump($_POST);
 ?>
 
 <!DOCTYPE html>
@@ -63,64 +57,67 @@ var_dump($_POST);
 </header>
 
 <?php
-if (isset($wrongInputs) && ($wrongInputs > 0)) { ?>
+if (isset($wrongInputs) && count($wrongInputs) > 0) { ?>
     <div class="error-message">
         <ul>
             <?php foreach ($wrongInputs as $wrongInput) { ?>
                 <li><?php echo $wrongInput; ?></li>
             <?php }; ?>
         </ul>
-    <?php }; ?>
     </div>
+<?php } else if ($_SERVER['REQUEST_METHOD'] === "POST") { ?>
+    <div class="success-message">
+        <?php echo 'Message bien reçu.' . PHP_EOL; ?>
+    </div>
+<?php }; ?>
 
+<body>
+    <h1 class="thunes">Toss a coin to your developer!</h1>
+    <main>
+        <section>
+            <img src="Geralt-gifEnGrand.gif"></img>
+            <form action='' method="POST">
+                <h2>Contactez-moi</h2>
+                <label for="name">Nom</label>
+                </br>
+                <input type="text" name="name" placeholder="Ecrivez votre nom ici" required>
+                </br>
+                <label for="email">Email</label>
+                </br>
+                <input type="email" name="email" placeholder="Ecrivez votre email ici" required>
+                </br>
+                <label for="Location">Région</label>
+                <select name="location" required>
+                    <option value="cintra">Cintra</option>
+                    <option value="novigrad">Novigrad</option>
+                    <option value="skellige">Skellige</option>
+                    <option value="temeria">Temeria</option>
+                    <option value="toussaint">Toussaint</option>
+                    <option value="velen">Velen</option>
+                </select>
 
-    <body>
-        <h1 class="thunes">Toss a coin to your developer!</h1>
-        <main>
-            <section>
-                <img src="Geralt-gifEnGrand.gif"></img>
-                <form action='' method="POST">
-                    <h2>Contactez-moi</h2>
-                    <label for="name">Nom</label>
-                    </br>
-                    <input type="text" name="name" placeholder="Ecrivez votre nom ici" required>
-                    </br>
-                    <label for="email">Email</label>
-                    </br>
-                    <input type="email" name="email" placeholder="Ecrivez votre email ici" required>
-                    </br>
-                    <label for="Location">Région</label>
-                    <select name="location" required>
-                        <option value="cintra">Cintra</option>
-                        <option value="novigrad">Novigrad</option>
-                        <option value="skellige">Skellige</option>
-                        <option value="temeria">Temeria</option>
-                        <option value="toussaint">Toussaint</option>
-                        <option value="velen">Velen</option>
-                    </select>
+                <label for="object"><Object>Objet</Object></label>
+                <select name="object" required>
+                    <option value="relative">Rechercher une personne disparue</option>
+                    <option value="monster">Chasser / Tuer un monstre</option>
+                    <option value="developer">Besoin d'un développeur web</option>
+                    <option value="ciri">Vous avez vu Ciri?</option>
+                    <option value="gwynt">Un petit Gwynt?</option>
+                    <option value="other">Autres </option>
+                </select>
 
-                    <label for="object"><Object>Objet</Object></label>
-                    <select name="object" required>
-                        <option value="relative">Rechercher une personne disparue</option>
-                        <option value="monster">Chasser / Tuer un monstre</option>
-                        <option value="developer">Besoin d'un développeur web</option>
-                        <option value="ciri">Vous avez vu Ciri?</option>
-                        <option value="gwynt">Un petit Gwynt?</option>
-                        <option value="other">Autres </option>
-                    </select>
+                <label for="message">Votre message</label>
+                <textarea name="message" value="message" placeholder="Ecrivez votre message ici..." style="height: 200px" required></textarea>
 
-                    <label for="message">Votre message</label>
-                    <textarea name="message" value="message" placeholder="Ecrivez votre message ici..." style="height: 200px" required></textarea>
-
-                    <div class="submit">
-                        <input type="submit" name="envoi" value="Envoyer">
-                    </div>
-                </form>
-            </section>
-        </main>
-    </body>
-    <footer>
-        <?php include('footer.php') ?>
-    </footer>
+                <div class="submit">
+                    <input type="submit" name="envoi" value="Envoyer">
+                </div>
+            </form>
+        </section>
+    </main>
+</body>
+<footer>
+    <?php include('footer.php') ?>
+</footer>
 
 </html>
